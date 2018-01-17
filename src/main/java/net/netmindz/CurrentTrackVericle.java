c7dc41ddbe1e9c021afbc5d5134da4a1d4d63f76
Will Tatam <will@netmindz.net>
2018-01-17 13:28:48 +0000

    Send track update to correct websocket and use proper json encoding

--- src/main/java/net/netmindz/CurrentTrackVericle.java
+++ src/main/java/net/netmindz/CurrentTrackVericle.java
@@ -12,6 +12,7 @@ import io.vertx.core.eventbus.Message;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import io.vertx.core.shareddata.LocalMap;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
@@ -32,16 +33,18 @@ public class CurrentTrackVericle extends AbstractVerticle {
 
         Handler<Message<JsonObject>> myHandler = new Handler<Message<JsonObject>>() {
             public void handle(Message<JsonObject> message) {
-                logger.info("I received a message " + message);
-                Collection<Object> chatters = vertx.sharedData().getLocalMap("chat.room").values();
-                logger.info("Sending track update to  " + chatters.size() +  " people in the chat room");
-                 for (Object chatter : chatters) {
+                logger.info("I received a message " + message.body());
+                LocalMap<String, String>  chatters = vertx.sharedData().getLocalMap("chat.room");
+                logger.info("Sending track update to " + chatters.size() +  " people in the chat room");
+                 for (Map.Entry<String, String> chatter : chatters.entrySet()) {
                     try {
-                        JsonObject object = message.body();
                         Map<String, String> current =  vertx.sharedData().getLocalMap("stream.metadata");
-                        String jsonOutput = "{\"sender\":\"HardHouseUK\", \"message\":\"New track is " + current.get("StreamTitle") + "\",\"received\":\""+new Date()+"\"}";
-                        logger.info("Sending message " + jsonOutput +  " to " + chatter);
-                        vertx.eventBus().send(chatter.toString(), jsonOutput);
+                        JsonObject updateMessage = new JsonObject();
+                        updateMessage.put("sender","HardHouseUK");
+                        updateMessage.put("message", "New track is " + current.get("StreamTitle"));
+                        updateMessage.put("received", new Date().toGMTString());
+                        logger.info("Sending message " + updateMessage.encode() +  " to " + chatter.getValue());
+                        vertx.eventBus().send(chatter.getKey(), updateMessage.encode());
                     }
                     catch(Exception e) {
                         logger.error("Failed to send track change to chat : " + e.getMessage(), e);
